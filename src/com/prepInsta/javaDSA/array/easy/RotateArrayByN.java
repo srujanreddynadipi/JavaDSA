b8f4134cbb7c119b89df5e1d26c527ae5865d33d
srujan reddy <srujanreddynsreddy@gmail.com>
2026-01-25 23:57:18 +0530

    RotateArray

--- src/com/prepInsta/javaDSA/array/easy/RotateArrayByN.java
+++ src/com/prepInsta/javaDSA/array/easy/RotateArrayByN.java
@@ -15,7 +15,7 @@ public class RotateArrayByN {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1,2,3,4,5,6,7,8};
         rotate(arr, 3);
 
         for (int num : arr) {
